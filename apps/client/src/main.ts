2923322d125022fbe9279f7e45e645936f78d70e
Em Ji Madhu <emjimadhu@gmail.com>
2020-01-04 17:16:23 +0530

    [CLIENT] Add `vuetify` UI Component

--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -1,13 +1,16 @@
 import Vue from 'vue';
+
 import App from './app.vue';
 import './register-service-worker';
 import router from './router';
 import store from './store';
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  vuetify,
+  render: (h) => h(App)
 }).$mount('#app');
