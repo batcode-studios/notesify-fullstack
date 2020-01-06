1d8bda0246d4012afc48b7e7b4d4cae213411e0a
Em Ji Madhu <emjimadhu@gmail.com>
2020-01-07 02:05:49 +0530

    [CLIENT] Refactor `app.component`

--- apps/client/src/app.component.ts
+++ apps/client/src/app.component.ts
@@ -2,14 +2,10 @@ import { Component, Vue } from 'vue-property-decorator';
 import { inject } from 'vue-typescript-inject';
 // @ts-ignore
 import WithRender from './app.component.html?style=./app.component.scss';
-import HelloWorldComponent from './components/hello-world.component';
 import { AppService } from './app.service';
 
 @WithRender
 @Component({
-  components: {
-    'hello-world': HelloWorldComponent
-  },
   providers: [AppService]
 })
 export default class AppComponent extends Vue {
