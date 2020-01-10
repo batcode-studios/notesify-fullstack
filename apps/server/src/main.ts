56c18d466f1bb84069e526a4ca42cf9b71ab988b
gnanamadhu <madhu@betatestsolutions.com>
2020-01-10 23:48:28 +0530

    [SERVER] Add nestjs boilerplate

---
+++ apps/server/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+import { environment } from './environments/environment';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(environment.port, () => {
+    console.log(`Listening at http://localhost:${environment.port}`);
+  });
+}
+bootstrap();
